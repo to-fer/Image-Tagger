be9f1866739a6b7e9d6709078e0c523c5c122b13
aferrero384 <aferrero384@gmail.com>
2014-03-13 09:32:33 -0700

    Made various vals in constructor private. Made database lazy.

--- src/main/scala/db/SlickTagDb.scala
+++ src/main/scala/db/SlickTagDb.scala
@@ -1,22 +1,22 @@
 import scala.slick.driver.SQLiteDriver.simple._
 
 object SlickTagDb {
-  class Tags(tag: Tag) extends Table[String](tag, "TAGS") {
+  private class Tags(tag: Tag) extends Table[String](tag, "TAGS") {
     def tagName = column[String]("TAG_NAME", O.PrimaryKey)
     override def * = tagName
   }
-  val tagTable = TableQuery[Tags]
+  private val tagTable = TableQuery[Tags]
 
-  class TagAliases(tag: Tag) extends Table[(String, String)](tag, "TAG_ALIASES") {
+  private class TagAliases(tag: Tag) extends Table[(String, String)](tag, "TAG_ALIASES") {
     def alias = column[String]("ALIAS")
     def aliasedTag = column[String]("ALIASED_TAG")
     def aliasedTagForeignKey = foreignKey("ALIASED_TAG_FK", aliasedTag, tagTable)(_.tagName)
     def aliasPrimaryKey = primaryKey("ALIAS_PK", (alias, aliasedTag))
     override def * = (alias, aliasedTag)
   }
-  val tagAliasesTable = TableQuery[TagAliases]
+  private val tagAliasesTable = TableQuery[TagAliases]
 
-  class TaggedFiles(tag: Tag) extends Table[(String, String)](tag, "TAGGED_FILES") {
+  private class TaggedFiles(tag: Tag) extends Table[(String, String)](tag, "TAGGED_FILES") {
     def path = column[String]("PATH")
     def tagName = column[String]("TAG_NAME")
     def tagNameForeignKey = foreignKey("TAG_NAME_FK", tagName, tagTable)(_.tagName)
@@ -24,9 +24,9 @@ object SlickTagDb {
     def taggedFileIndex = index("TAGGED_FILE_IDX", (path, tag), unique = true)
     def * = (path, tagName)
   }
-  val taggedFilesTable = TableQuery[TaggedFiles]
+  private val taggedFilesTable = TableQuery[TaggedFiles]
 
-  val database = Database.forURL("jdbc:sqlite:db.sqlite", driver = "org.sqlite.JDBC")
+  private lazy val database = Database.forURL("jdbc:sqlite:db.sqlite", driver = "org.sqlite.JDBC")
   database withSession {
     implicit session =>
     (tagTable.ddl ++ tagAliasesTable.ddl ++ taggedFilesTable.ddl).create
