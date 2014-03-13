a1b07bc21d44599b2ceeb6aa9a832f21f364c12f
aferrero384 <aferrero384@gmail.com>
2014-03-13 01:44:11 -0700

    Added foreign keys in relevant places.

--- src/main/scala/db/SlickTagDb.scala
+++ src/main/scala/db/SlickTagDb.scala
@@ -10,6 +10,7 @@ object SlickTagDb {
   class TagAliases(tag: Tag) extends Table[(String, String)](tag, "TAG_ALIASES") {
     def alias = column[String]("ALIAS")
     def aliasedTag = column[String]("ALIASED_TAG")
+    def aliasedTagForeignKey = foreignKey("ALIASED_TAG_FK", aliasedTag, tagTable)(_.tagName)
     def aliasPrimaryKey = primaryKey("ALIAS_PK", (alias, aliasedTag))
     override def * = (alias, aliasedTag)
   }
@@ -18,6 +19,7 @@ object SlickTagDb {
   class TaggedFiles(tag: Tag) extends Table[(String, String)](tag, "TAGGED_FILES") {
     def path = column[String]("PATH")
     def tagName = column[String]("TAG_NAME")
+    def tagNameForeignKey = foreignKey("TAG_NAME_FK", tagName, tagTable)(_.tagName)
     def taggedFilePrimaryKey = primaryKey("TAGGED_FILE_PK", (path, tag))
     def taggedFileIndex = index("TAGGED_FILE_IDX", (path, tag), unique = true)
     def * = (path, tagName)
