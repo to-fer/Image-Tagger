2d8beef03b48a34c655498441f74d3cb7a1b02a2
to-fer <tony.j.ferrero@gmail.com>
2014-04-04 00:30:47 -0700

    Removed Container code.

--- src/main/scala/qt/gui/Layout.scala
+++ src/main/scala/qt/gui/Layout.scala
@@ -1,16 +1,12 @@
 package qt.gui
 
 trait Layout extends Parent {
-  private[gui] var containerMap = Map[Widget, Container]()
 
   protected def layout(w: Widget)
 
   override def +=(w: Widget) = {
     super.+=(w)
-
-    val containedWidget = new Container(w)
-    containerMap = containerMap + (w -> containedWidget)
-    layout(containedWidget)
+    layout(w)
   }
 
 }
