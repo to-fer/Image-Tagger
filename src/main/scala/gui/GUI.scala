499fa67ac63ecf59ce3009c3b911b1b3501fd461
to-fer <tony.j.ferrero@gmail.com>
2014-04-04 00:38:27 -0700

    More formatting nicer.

--- src/main/scala/gui/GUI.scala
+++ src/main/scala/gui/GUI.scala
@@ -13,30 +13,7 @@ import db.SlickTagDb
 
 object GUI extends QtApp {
 
-  override val mainWindow = new Window {
-    title = "Tagger"
-    maximized = true
-  }
-
-  val (screenWidth, screenHeight) = Screen.size
-  val searchWidget = new ScrollWidget {
-    width = screenWidth
-    height = screenHeight
-  }
-  val imageWidget = new StackedWidget {
-    width = screenWidth
-    height = screenHeight
-  }
-  val displayStack = new StackedWidget {
-    width = screenWidth
-    height = screenHeight
-    stackingMode = StackingMode.StackOne
-    content = List (
-      searchWidget,
-      imageWidget
-    )
-  }
-
+  // Non-GUI initialization stuff
   val tagDb = new SlickTagDb("db.sqlite")
   var knownTags = tagDb.tags match {
     case Some(tags) => tags.toSet
@@ -51,114 +28,138 @@ object GUI extends QtApp {
   if (!Files.exists(imageDest))
     Files.createDirectory(imageDest)
 
-  val lineEdit: LineEdit = new LineEdit {
-    width = 400
-    height = 25
-    alignment = List(AlignmentFlag.AlignHCenter, AlignmentFlag.AlignBottom)
-    styleSheet = "background: white;"
-    focus()
+  val (screenWidth, screenHeight) = Screen.size
 
-    val commandEntered: () => Unit = () => {
-      def onCommand() = {
-        val enteredCommand = text
-        text = ""
-        enteredCommand
-      }
+  override val mainWindow = new Window {
+    title = "Tagger"
+    maximized = true
 
-      def errorMessage(msg: String): Unit = {
-        text = msg
-        lineEdit.selectAll()
-      }
-      val enteredCommand = onCommand()
+    val searchWidget = new ScrollWidget {
+      width = screenWidth
+      height = screenHeight
+    }
+    val imageWidget = new StackedWidget {
+      width = screenWidth
+      height = screenHeight
+    }
 
-      enteredCommand match {
-        case "tag" => {
-          val imageFiles = Image.imageFilesIn(imageDir.toString)
-          if (!imageFiles.isEmpty) {
-            displayStack.currentWidget = imageWidget
+    val displayStack = new StackedWidget {
+      width = screenWidth
+      height = screenHeight
+      stackingMode = StackingMode.StackOne
+      content = List (
+        searchWidget,
+        imageWidget
+      )
+    }
 
-            val viewer = new SequentialImageViewer(
-              layout = imageWidget,
-              imageFiles = imageFiles,
-              imageWidth = screenWidth,
-              imageHeight = screenHeight
-            )
-            viewer.showFirstImage()
+    content = List (
+      new Container(new LineEdit {
+        width = 400
+        height = 25
+        alignment = List(AlignmentFlag.AlignHCenter, AlignmentFlag.AlignBottom)
+        styleSheet = "background: white;"
+        focus()
 
-            returnPressed = () => {
-              val enteredCommand = onCommand()
+        val commandEntered: () => Unit = () => {
+          def onCommand() = {
+            val enteredCommand = text
+            text = ""
+            enteredCommand
+          }
 
-              def showNextImageIfExists() =
-                if (viewer.hasNext)
-                  viewer.showNextImage()
-                else
-                  errorMessage("All images have been tagged.")
+          def errorMessage(msg: String): Unit = {
+            text = msg
+            selectAll()
+          }
+          val enteredCommand = onCommand()
 
-              if (enteredCommand != "") {
-                enteredCommand match {
-                  case SkipCommand(_) => showNextImageIfExists()
-                  case AddTagCommand(tag) => {
-                    if (tag.contains(" "))
-                      errorMessage("Tags cannot contain spaces.")
-                    else if (knownTags.contains(tag))
-                      errorMessage("That tag already exists.")
-                    else {
-                      knownTags = knownTags + tag
-                      tagDb.addTag(tag)
+          enteredCommand match {
+            case "tag" => {
+              val imageFiles = Image.imageFilesIn(imageDir.toString)
+              if (!imageFiles.isEmpty) {
+                displayStack.currentWidget = imageWidget
+
+                val viewer = new SequentialImageViewer(
+                  layout = imageWidget,
+                  imageFiles = imageFiles,
+                  imageWidth = screenWidth,
+                  imageHeight = screenHeight
+                )
+                viewer.showFirstImage()
+
+                returnPressed = () => {
+                  val enteredCommand = onCommand()
+
+                  def showNextImageIfExists() =
+                    if (viewer.hasNext)
+                      viewer.showNextImage()
+                    else
+                      errorMessage("All images have been tagged.")
+
+                  if (enteredCommand != "") {
+                    enteredCommand match {
+                      case SkipCommand(_) => showNextImageIfExists()
+                      case AddTagCommand(tag) => {
+                        if (tag.contains(" "))
+                          errorMessage("Tags cannot contain spaces.")
+                        else if (knownTags.contains(tag))
+                          errorMessage("That tag already exists.")
+                        else {
+                          knownTags = knownTags + tag
+                          tagDb.addTag(tag)
+                        }
+                      }
+                      case TagCommand(tags) if (tags.forall(knownTags.contains(_))) => {
+                        val imageFile = viewer.currentImageFile
+                        val destFile = imageDest resolve imageFile.toPath.getFileName
+                        tagDb.tagFile(destFile.toString, tags)
+                        showNextImageIfExists()
+                        Files.move(imageFile.toPath, destFile)
+                      }
+                      case "delete" => {
+                        val curImageFile = viewer.currentImageFile
+                        val curImage = viewer.getCurrentImage
+                        curImage.dispose()
+                        curImageFile.delete()
+                        viewer.showNextImage()
+                      }
+                      case QuitCommand(_) => {
+                        viewer.dispose()
+                        returnPressed = commandEntered
+                        displayStack.currentWidget = searchWidget
+                      }
+                      case _ =>
+                        errorMessage("Unknown command.")
                     }
                   }
-                  case TagCommand(tags) if (tags.forall(knownTags.contains(_))) => {
-                    val imageFile = viewer.currentImageFile
-                    val destFile = imageDest resolve imageFile.toPath.getFileName
-                    tagDb.tagFile(destFile.toString, tags)
-                    showNextImageIfExists()
-                    Files.move(imageFile.toPath, destFile)
-                  }
-                  case "delete" => {
-                    val curImageFile = viewer.currentImageFile
-                    val curImage = viewer.getCurrentImage
-                    curImage.dispose()
-                    curImageFile.delete()
-                    viewer.showNextImage()
-                  }
-                  case QuitCommand(_) => {
-                    viewer.dispose()
-                    returnPressed = commandEntered
-                    displayStack.currentWidget = searchWidget
-                  }
-                  case _ =>
-                    errorMessage("Unknown command.")
                 }
               }
+              else
+                errorMessage("There are no images to tag.")
+            }
+            case tag => {
+              if (knownTags.contains(tag)) {
+                val taggedImageFiles = tagDb.filesWithTag(tag)
+                val gridWidget = new GridWidget
+                val imageWidth = screenWidth/5
+                val imageHeight = screenHeight/5
+                taggedImageFiles foreach { f => {
+                  val image = new Image(f.toString, imageWidth, imageHeight) {
+                    frameShape = QFrame.Shape.Box
+                  }
+                  gridWidget += image
+                }}
+                searchWidget.content = gridWidget
+              }
+              else
+                errorMessage("Invalid tag.")
             }
           }
-          else
-            errorMessage("There are no images to tag.")
         }
-        case tag => {
-          if (knownTags.contains(tag)) {
-            val taggedImageFiles = tagDb.filesWithTag(tag)
-            val gridWidget = new GridWidget
-            val imageWidth = screenWidth/5
-            val imageHeight = screenHeight/5
-            taggedImageFiles foreach { f => {
-              val image = new Image(f.toString, imageWidth, imageHeight) {
-                frameShape = QFrame.Shape.Box
-              }
-              gridWidget += image
-            }}
-            searchWidget.content = gridWidget
-          }
-          else
-            errorMessage("Invalid tag.")
-        }
-      }
-    }
-    returnPressed = commandEntered
+        returnPressed = commandEntered
+      }),
+      new Container(displayStack)
+    )
   }
-
-  mainWindow content = List (
-    new Container(lineEdit),
-    new Container(displayStack)
-  )
 }
