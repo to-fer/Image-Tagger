b15477bc38c7571868bd261b5f5c8194ba6700d8
to-fer <tony.j.ferrero@gmail.com>
2014-05-25 22:16:02 -0700

    Purge whitespace.

--- src/main/scala/event/mode/ModeSwitcher.scala
+++ src/main/scala/event/mode/ModeSwitcher.scala
@@ -4,10 +4,6 @@ import qt.gui.Widget
 import command.CommandResult
 
 class ModeSwitcher(activeMode: ActiveMode, modeViewMap: Map[Mode, Widget]) {
-  
-  def switch(newMode: Mode): Unit = {
+  def switch(newMode: Mode): Unit =
     activeMode.setModeAndView(newMode, modeViewMap(newMode))
-  }
-
-
 }
