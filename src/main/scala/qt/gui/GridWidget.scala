c0bb50bbda3112ffdd2fbe94f2672a89aa63fc2a
to-fer <tony.j.ferrero@gmail.com>
2014-05-30 23:17:50 -0700

    Add spacing setter/getter.

--- src/main/scala/qt/gui/GridWidget.scala
+++ src/main/scala/qt/gui/GridWidget.scala
@@ -4,9 +4,18 @@ import com.trolltech.qt.gui.QGridLayout
 
 class GridWidget extends Widget with Layout {
   private val gridLayout = new QGridLayout(delegate)
-  gridLayout.setSpacing(0)
-
+  private var _spacing = 0
   private var _maxColumns = 5
+
+  // Make sure we call setSpacing to default spacing to 0
+  spacing = 0
+
+  def spacing = _spacing
+  def spacing_=(s: Int) = {
+    _spacing = s
+    gridLayout.setSpacing(s)
+  }
+
   def maxColumns = _maxColumns
   def maxColumns_=(maxColumns: Int) =
     if (maxColumns != _maxColumns) {
